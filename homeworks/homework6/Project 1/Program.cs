34318a7a36262a746d536656d4aa245f609e47dc
RomanW <fackloar@gmail.com>
2021-02-09 03:13:55 +0300

    main

---
+++ homeworks/homework6/Project 1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TaskManager
+{
+    class Program
+    {
+        public enum ErrorCodes
+        {
+        NoProcessFound
+        }
+
+        public class NameException : Exception
+        {
+            public NameException(ErrorCodes code)
+            {
+                Code = code;
+            }
+
+            public ErrorCodes Code { get; }
+        }
+
+        static public void GetProcessList()
+        {
+            System.Diagnostics.Process[] curProcesses = Process.GetProcesses();
+            for (int i = 0; i < curProcesses.GetLength(0); i++)
+            {
+                Console.WriteLine(curProcesses[i]);
+                Console.WriteLine($"        ID: {curProcesses[i].Id.ToString()}");
+            }
+        }
+        static public void KillProcess()
+        {
+            Console.WriteLine("Now you can end any process. \nWrite its name or its ID.\nNote that if you end process by its name it will end ALL process of the same name!");
+            input:
+            string userInput = Console.ReadLine();
+            bool chars = Int32.TryParse(userInput, out int id);
+                
+                if (chars != true)
+                {
+                    try 
+                    {
+                        Process[] processToKill = Process.GetProcessesByName(userInput);
+                        if (processToKill == null || processToKill.Length == 0)
+                        {
+                            throw new NameException(ErrorCodes.NoProcessFound);
+                        }
+                        else
+                        {
+                            for (int i = 0; i < processToKill.GetLength(0); i++)
+                            processToKill[i].Kill();
+                            Console.WriteLine($"All processes by the name {userInput} are succesfully ended");
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Something wrong with your input.\nWrite name or ID.");
+                        goto input;
+                    }
+                }
+                else if (chars)
+                {
+                    Process processToKill = Process.GetProcessById(id);
+                    processToKill.Kill();
+                    Console.WriteLine($"All processes by ID: {id} succesfully ended");
+                    }
+        }
+
+        static public void userChoice()
+        {
+            choice:
+            Console.WriteLine("Input 1 to get updated list of processes.\nInput 2 to end a process.\nInput 'q' to quit.");
+            string userChoice = Console.ReadLine();
+            if (userChoice == "q")
+            {
+                Process ThisApp = Process.GetCurrentProcess();
+                ThisApp.Kill();
+            }
+            bool inputNumbers = Int32.TryParse(userChoice, out int option);
+            if (option == 1)
+            {
+                GetProcessList();
+                goto choice;
+            }
+            else if (option == 2)
+            {
+                KillProcess();
+                goto choice;
+            }
+            else
+            {
+                Console.WriteLine("Something wrong with your input.");
+                goto choice;
+            }
+        }
+            
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Task Manager.\nYou will see all the processes running on the computer and their ID.");
+            GetProcessList();
+            userChoice();
+            
+        }
+    }
+}
---
+++ homeworks/homework7/Project 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Project_1
+{
+    class Program
+    {
+     
+        static public bool EqualSlices(int totalSlices, int peopleNumber, int slicesEach)
+        {
+            bool equalSlices = true;
+            int slicesGot = peopleNumber * slicesEach;
+            if (totalSlices >= slicesGot)
+            {
+                equalSlices = true;
+            }
+            else if (totalSlices < slicesGot)
+            {
+                equalSlices = false;
+            }
+            else if (peopleNumber == 0)
+            {
+                equalSlices = true;
+            }
+            return equalSlices; 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's see if it's possible to divide a pie evenly!");
+            Console.WriteLine("Input how many slices you have in total.");
+            string totalSlices = Console.ReadLine();
+            int totalSlicesInt = Int32.Parse(totalSlices);
+            Console.WriteLine("Input how many people will eat the pie.");
+            string peopleNumber = Console.ReadLine();
+            int peopleNumberInt = Int32.Parse(peopleNumber);
+            Console.WriteLine("Input how many slices should each person get.");
+            string slicesEach = Console.ReadLine();
+            int slicesEachInt = Int32.Parse(slicesEach);
+            bool answer = EqualSlices(totalSlicesInt, peopleNumberInt, slicesEachInt);
+            if (answer == true)
+            {
+                Console.WriteLine("You can divide a pie evenly!");
+            }
+            else
+            {
+                Console.WriteLine("You can't divide a slice evenly");
+            }
+            Console.ReadKey();
+        }
+    }
+}
