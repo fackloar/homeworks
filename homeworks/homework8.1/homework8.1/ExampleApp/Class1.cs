e034caf69ac0b80813ed3e7ddd51de32f3ae3e0d
fackloar <fackloar@gmail.com>
2021-02-16 12:55:30 +0300

    main

---
+++ homeworks/homework8.1/homework8.1/ExampleApp/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp
+{
+    public class Greeting
+    {
+        public static void SayHello()
+        {
+            System.Console.WriteLine("Hello from ExampleLibrary!");
+        }
+
+    }
+}
---
+++ homeworks/homework8.1/homework8.1/homework8.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExampleApp;
+
+namespace homework8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greeting.SayHello();
+            if (string.IsNullOrEmpty(Properties.Settings.Default.userName))
+            {
+
+                Console.WriteLine("Enter user name:");
+                Properties.Settings.Default.userName = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Job))
+            {
+
+                Console.WriteLine("What is your job?");
+                Properties.Settings.Default.Job = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Age))
+            {
+
+                Console.WriteLine("How old are you?");
+                Properties.Settings.Default.Age = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            string userName = Properties.Settings.Default.userName;
+            string job = Properties.Settings.Default.Job;
+            string age = Properties.Settings.Default.Age;
+            string greeting = Properties.Settings.Default.Greeting;
+            Console.WriteLine($"{greeting}, {userName}!\nYour job is {job}. You are {age} years old");
+            Console.ReadKey();
+        }
+    }
+}
