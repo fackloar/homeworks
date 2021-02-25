30716ad3dae73f73156a804beafd1ec314811762
fackloar <fackloar@gmail.com>
2021-02-25 04:57:39 +0300

    homework2

---
+++ homeworks/c# algorithms/homework2/Project 1/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace Project_1
+{
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+    }
+
+
+
+    //Начальную и конечную ноду нужно хранить в самой реализации интерфейса
+    public interface ILinkedList
+    {
+        int GetCount(); // возвращает количество элементов в списке
+        void AddNode(int value);  // добавляет новый элемент списка
+        void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+        void RemoveNode(int index); // удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); // удаляет указанный элемент
+        Node FindNode(int searchValue); // ищет элемент по его значению
+    }
+
+    public class ImpClass : ILinkedList
+    {
+        Node firstNode = new Node();
+        Node lastNode = new Node();
+
+        public void AddNode(int value)
+        {
+            var newNode = new Node { Value = value };
+            var last = firstNode;
+            newNode.NextNode = null;
+
+            if (firstNode == null)
+            {
+                newNode.PrevNode = null;
+                firstNode = newNode;
+                return;
+            }
+
+            while (last.NextNode != null)
+                last = last.NextNode;
+
+            last.NextNode = newNode;
+            newNode.PrevNode = last;
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            var newNode = new Node { Value = value };
+            newNode.NextNode = node.NextNode;
+            node.NextNode = newNode;
+            newNode.PrevNode = node;
+
+            if (newNode.NextNode != null)
+                newNode.NextNode.PrevNode = newNode;
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            Node temp = firstNode;
+            int pos = 0;
+
+            while (temp.Value != searchValue && temp.NextNode != null)
+            {
+                pos++;
+                temp = temp.NextNode;
+            }
+
+            return (temp);
+        }
+
+        public int GetCount()
+        {
+            Node temp = new Node();
+            temp = this.firstNode;
+            int i = 0;
+
+            while (temp != null)
+            {
+                i++;
+                temp = temp.NextNode;
+            }
+
+            return i;
+        }
+
+        public void RemoveNode(Node node)
+        {
+            if (firstNode == null || node == null)
+            {
+                return;
+            }
+
+            if (firstNode == node)
+            {
+                firstNode = node.NextNode;
+            }
+
+            if (node.NextNode != null)
+            {
+                node.NextNode.PrevNode = node.PrevNode;
+            }
+
+            if (node.PrevNode != null)
+            {
+                node.PrevNode.NextNode = node.NextNode;
+            }
+
+            return;
+        }
+        public void RemoveNode(int index)
+        {
+            if (firstNode == null || index <= 0)
+                return;
+
+            Node current = firstNode;
+            int i;
+
+            for (i = 1; current != null && i < index; i++)
+            {
+                current = current.NextNode;
+            }
+
+            if (current == null)
+                return;
+
+            RemoveNode(current);
+        }
+
+
+        public void TraverseFront(Node node)
+
+        {
+
+            while (node != null)
+
+            {
+
+               Console.Write($"{node.Value}, ");
+
+                node = node.NextNode;
+
+            }
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var List = new ImpClass();
+            int[] Test = { 1, 20, 300, 4000, 50000 };
+            for (int i =0; i < Test.Length; i++)
+            {
+                List.AddNode(Test[i]);   // list: 0, 1, 20, 300, 4000, 50000
+            }                      
+            
+            int count = List.GetCount();
+            int expectedCount = 6;
+            Console.WriteLine($"expected result of GetCount: {expectedCount}\nactual result of GetCount: {count}");
+
+            List.AddNodeAfter(List.FindNode(300), 500); // list: 0, 1, 20, 300, 500, 4000, 50000
+            List.RemoveNode(6);  // list: 0, 1, 20, 300, 500, 50000
+
+            int[] expectedArray = { 0, 1, 20, 300, 500, 50000 };
+            Console.WriteLine("expected list: ");
+            for (int i = 0; i < expectedArray.Length; i++)
+            {
+                Console.Write($"{expectedArray[i]}, ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("actual list: ");
+            List.TraverseFront(List.FindNode(0));
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ homeworks/c# algorithms/homework2/Project 2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Project_2
+{
+    class Program
+    {
+        public class Test
+        {
+            public int[] testArray { get; set; }
+            public int testValue { get; set; }
+            public int expected { get; set; }
+        }
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1; // O(1)
+        }
+
+        static void Main(string[] args)
+        {
+            var testData = new Test[5];
+            testData[0] = new Test()
+            {
+                testArray = new int[5] {1, 2, 3, 4, 5},
+                testValue = 3,
+                expected = 2,
+            };
+            testData[1] = new Test()
+            {
+                testArray = new int[10] { 10, 100, 5, 200, 15, 350, 2000, 4000, 154, 46 },
+                testValue = 46,
+                expected = -1,
+            };
+            testData[2] = new Test()
+            {
+                testArray = new int[3] { 10, 100, 1000 },
+                testValue = 100,
+                expected = 1,
+            };
+            testData[3] = new Test()
+            {
+                testArray = new int[6] { 3, 6, 9, 12, 15, 18 },
+                testValue = 6,
+                expected = 1,
+            };
+            testData[4] = new Test()
+            {
+                testArray = new int[7] { 10, 20, 30, 40, 50, 60, 70 },
+                testValue = 60,
+                expected = 5,
+            };
+            int i = 0;
+            foreach (var Test in testData)
+            {
+                i++;
+                int result = BinarySearch(Test.testArray, Test.testValue);
+                Console.WriteLine($"Test {i}\ntestArray:{Test.testArray}'\ntestValue: {Test.testValue}\nexpected: '{Test.expected}'\nresult: '{result}'");
+                Console.WriteLine();
+            }
+        }
+    }
+}
